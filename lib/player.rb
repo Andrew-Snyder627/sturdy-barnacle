ffc3d3b5cd0aef4a1307949e6648cc07305eb300
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-02-13 13:46:00 -0500

    Add notes from IC review during lesson

--- lib/player.rb
+++ lib/player.rb
@@ -1,28 +1,31 @@
 class Player
-    attr_reader :full_name, :monthly_cost, :contract_length
+    attr_reader :full_name, :monthly_cost, :contract_length, :nickname
 
     def initialize(full_name, monthly_cost, contract_length)
         @full_name = full_name
         @monthly_cost = monthly_cost
         @contract_length = contract_length
-        @nickname = nil
+        # @nickname = nil #This actually is not needed either, default will be nil as is
     end
 
     def first_name #splitting first name out of full name
-        full_name.split(" ",2).first
+        full_name.split[0] #better way to write this
+        # full_name.split(" ",2).first
     end
 
     def last_name #splitting last name out of full name
-        full_name.split(" ", 2).last
+        full_name.split[1] #better way to write this
+        # full_name.split(" ", 2).last
     end
 
     def total_cost #monthly cost multiplied by contract length (length in months per the README)
         @monthly_cost * @contract_length
     end
 
-    def nickname #returns the nickname if it is set, nil otherwise. Do I need this? Curious if setting it above is doing this anyway
-        @nickname
-    end
+    # def nickname #returns the nickname if it is set, nil otherwise. Do I need this? Curious if setting it above is doing this anyway
+    #     @nickname
+    # end
+    #Did not need this method once added to the attr_reader
 
     def set_nickname!(nickname) #sets the nickname if given one
         @nickname = nickname
--- lib/team.rb
+++ lib/team.rb
@@ -16,15 +16,44 @@ class Team
     end
 
     def long_term_players #selecting the players with contract_length greater than 24
-        @roster.select {|player| player.contract_length > 24}
+        # @roster.select {|player| player.contract_length > 24}
+
+        #Different way to write this
+        # found_players = [] #here to grab the player.contract_length > 24
+        # @roster.each do |player|
+        #     if player.contract_length > 24
+        #         found_players << player
+        #     end
+        # end
+        # found_players
+
+        #another way probably best for this use case
+        @roster.find_all do |player|
+            player.contract_length > 24
+        end
     end
 
     def short_term_players #selecting the players with contract_length less than or equal to 24
-       @roster.select {|player| player.contract_length <= 24}
+    #    @roster.select {|player| player.contract_length}
+        @roster.find_all do |player|
+            player.contract_length <= 24
+        end
     end
 
     def total_value #taking the total_cost of each player in the array and summing for total_value
-        @roster.sum {|player| player.total_cost}
+        # @roster.sum {|player| player.total_cost}
+
+        #another way to write
+        @roster.sum do |player|
+            player.total_cost
+        end
+
+        # #another way to write
+        # total = 0
+        # @roster.each do |player|
+        #     total += player.total_cost
+        # end
+        # total
     end
 
     def details
